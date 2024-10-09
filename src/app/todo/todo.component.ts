b50a01e18cd8e61bd6a56f8820dcd4b1676f7a38
HabeebR07 <habeebrahmanca07@gmail.com>
2024-10-09 22:48:24 +0530

    added the logic and template, css left

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,14 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
+
 export class TodoComponent {
-  
+  todos: Todo[] = []
+  newTodoInput: string = ''
+
+  // adding Todo
+  addTodo(){
+    if(this.newTodoInput.trim()){
+      const newTodo: Todo = {
+        title: this.newTodoInput,
+        completed: false
+      }
+      this.todos.push(newTodo)
+      this.newTodoInput = ''
+    }
+  }
+
+  //deleting Todo
+  deleteTodo(index : number){
+    this.todos.splice(index, 1)
+  }
+
+  //toggle Todo
+  toggleStatus(index: number){
+    this.todos = this.todos.map((todo, i) => i === index ? {...todo, completed: !todo.completed} : todo)
+  }
 }
+
